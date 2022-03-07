e786cd3582e1b8a526d14cef2640653b6f785114
root <root@localhost.localdomain>
2022-03-08 01:03:53 +0800

    rename gridMap

--- src/gridMap.h
+++ src/gridMap.h
@@ -2,13 +2,15 @@
 #include <set>
 
 #define MAX_MAP_SIZE 10
+#define OBSTACLE_COUNT 5
+
 using namespace std;
 struct Spot
 {
     int m_x;
     int m_y;
-    bool is_obstacle;
-    Spot(int x, int y):m_x(x), m_y(y), is_obstacle(false){};
+    // bool is_obstacle;
+    Spot(int x, int y):m_x(x), m_y(y)/*, is_obstacle(false)*/{};
     bool operator == (Spot spot){return spot.m_x == m_x && spot.m_y == m_y;};
 };
 
